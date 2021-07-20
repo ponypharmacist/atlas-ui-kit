2c4daf41acad1846b62dac9ba3ae4581fab1b082
Dmitry Glinskiy <glinskiy.dmitry@gmail.com>
2021-07-20 10:22:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scoped репозиторий с компонентами Ui Kit
+
+Штука для штук.
+
+## Project setup
+```
+yarn add @ponypharmacist/atlas-ui-kit
+```
---
+++ src/components/AuInput/AuInput.vue
@@ -0,0 +1,59 @@
+<template>
+
+<input
+  v-model="model"
+  :type="type"
+  :disabled="disabled"
+  class="au-input"
+  ref="input"
+>
+
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('AuInput', {
+  name: 'AuInput',
+
+  props: {
+    value: {
+      type: [String, Number],
+      default: null,
+    },
+    type: {
+      type: String,
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    regex: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        if (val && this.regex) {
+          if (new RegExp('^' + this.regex + '$').test(val)) {
+            this.$emit('input', val);
+          } else {
+            this.$refs.input.value = this.value || '';
+          }
+        } else {
+          this.$emit('input', val);
+        }
+      }
+    }
+  }
+})
+</script>
+
+<style></style>
